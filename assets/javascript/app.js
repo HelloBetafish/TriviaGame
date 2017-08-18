c421059e4acc187cc61500f9aa804e4e13d4e2f8
HelloBetafish <bethany.pfeister@gmail.com>
2017-08-18 16:26:56 -0700

    current version

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,32 +15,36 @@ var countdown = function(){
   }
 }
 
-var start = function(){
+var timer = function(){
   if (!timeOut){
   	time = 30;
     intervalId = setInterval(countdown,1000)
   }
 }
 
-start();
+timer();
 
 // =================================
 // Questions
 var questionArray = [
   {question: "This is the first Question.",
 	choices: ["Choice a", "Choice b", "Choice c", "Choice d"],
-	correctChoice: "Choice c"},
+	correctChoice: "Choice c",
+	img: "test.jpeg"},
   {question: "This is the second Question.",
 	choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
-	correctChoice: "Choice 1"},
+	correctChoice: "Choice 1",
+	img: "test2.jpeg"},
   {question: "This is the third Question.",
   choices: ["Choice i", "Choice ii", "Choice iii", "Choice iv"],
-  correctChoice: "Choice iv"}
+  correctChoice: "Choice iv",
+  img: "test3.jpeg"}
 ]
 // ==================================
 // Counters
 var numCorrect = 0;
 var numIncorrect = 0;
+var unanswered = 0;
 
 // ==============================
 // Create radio buttons populated with choices
@@ -60,7 +64,7 @@ var displayQnA = function(currentQ){
 
 }
 
-displayQnA(questionArray[0]);
+// displayQnA(questionArray[0]);
 
 // ========================================
 // Check if correct answer checked
@@ -69,10 +73,14 @@ var isCorrectChoice = function (currentQ){
   var userSelected = $('input[name=radioSelect]:checked','#choices').val();
   if(userSelected === currentQ.correctChoice){
     numCorrect++;
+    $("#question").text("Correct!");
+    console.log(numCorrect);
     return true;
 	}
   else{
   	numIncorrect++;
+  	$("#question").text("Incorrect!");
+  	console.log(numIncorrect);
   	return false;
   }
 }
@@ -80,11 +88,26 @@ var isCorrectChoice = function (currentQ){
 // ============================
 // Make submit button check response
 $("#verify").on("click", function() {
-  isCorrectChoice(currentQ);
-  if(isCorrectChoice){
-  	$("#question").text("Correct!");
+  isCorrectChoice(questionArray[0]);
+  if(isCorrectChoice === true){
+  	console.log("Correct!");
+  	// $("#question").text("Correct!");
   }
   else{
-  	$("#question").text("Incorrect!");
+  	console.log("Incorrect!");
+  	// $("#question").text("Incorrect!");
   }
 });
+
+// ================
+// Test game
+
+for(var i = 0; i < questionArray.length; i++){
+	var currentQ = questionArray[i];
+	displayQnA(currentQ);
+	console.log(currentQ);
+}
+
+var y = 0;
+y++
+displayQnA(questionArray[y]);
