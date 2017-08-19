5e723a5954ebc5b9bf6123dc2ed0553bdfac3a6b
HelloBetafish <bethany.pfeister@gmail.com>
2017-08-18 20:27:14 -0700

    minor fixes, still needs content and styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,6 +3,12 @@ var time = 30;
 var intervalId;
 var timeOut = false;
 
+var timer = function(){
+  if (!timeOut){
+    intervalId = setInterval(countdown,1000)
+  }
+}
+
 var countdown = function(){
   time--;
   $("#timer").text("Time Remaining: " + time);
@@ -14,7 +20,70 @@ var countdown = function(){
   }
 }
 
+// Counters
+var numCorrect = 0;
+var numIncorrect = 0;
+var unanswered = 0;
+var count = 0;
+var answer = "";
+var imgSrc = "";
+
+// =================================
+// Questions for game
+var questionArray = [
+  {question: "This is the first Question.",
+  choices: ["Choice a", "Choice b", "Choice c", "Choice d"],
+  correctChoice: "Choice c",
+  image: "https://static.fjcdn.com/gifs/Cat_439f66_783457.gif"},
+  {question: "This is the second Question.",
+  choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
+  correctChoice: "Choice 1",
+  image: "http://www.dream-wallpaper.com/free-wallpaper/nature-wallpaper/waterfall-and-stream-2-wallpaper/1280x1024/free-wallpaper-11.jpg"},
+  {question: "This is the third Question.",
+  choices: ["Choice i", "Choice ii", "Choice iii", "Choice iv"],
+  correctChoice: "Choice iv",
+  image: "http://images.all-free-download.com/images/graphiclarge/beautiful_natural_scenic_03_hd_picture_166230.jpg"}
+]
+// ==================================
+// Function called to start off the game
+
 var reset = function(){
+numCorrect = 0;
+numIncorrect = 0;
+unanswered = 0;
+count = 0;
+answer = "";
+imgSrc = "";
+timeOut = false;
+time = 30;
+$("#choices").empty();
+$("#timer").text("Time Remaining: " + time);
+timer();
+displayQnA(questionArray[count]);
+}
+
+// Function to create radio buttons populated with choices and Submit button
+
+var displayQnA = function(currentQ){
+  $("#question").text(currentQ.question);
+  for(var i = 0; i < currentQ.choices.length; i++){
+ 
+  var option =$("<label>");
+  option.html("<input type='radio' name='radioSelect' value ='" + 
+  currentQ.choices[i] + "'>" + currentQ.choices[i]);
+  $("#choices").append(option);
+  }
+  $("label").after("</br>");
+  var btn = $("<button id='verify' type='button'>").addClass("btn btn-lg btn-primary").text("Submit");
+  $("#choices").append(btn);
+  answer = currentQ.correctChoice;
+  imgSrc = currentQ.image;
+}
+
+// ============================
+// Function to pull next question
+
+var nextQ = function(){
   $("#choices").empty();
   count++;
   if(count<questionArray.length){
@@ -30,18 +99,12 @@ var reset = function(){
   	$("#choices").append("<p>Correct Answers: " + numCorrect + "</p>");
     $("#choices").append("<p>Incorrect Answers: " + numIncorrect + "</p>");
     $("#choices").append("<p>Unanswered Questions: " + unanswered + "</p>");
-    var btn = $("<button type='button'>").addClass("btn btn-lg btn-primary start").text("Try again?");
+    var btn = $("<button type='button'>").addClass("btn btn-lg btn-primary reset").text("Try again?");
     $("#choices").append(btn);
   }
 }
 
-
-var timer = function(){
-  if (!timeOut){
-    intervalId = setInterval(countdown,1000)
-  }
-}
-
+// Function to display correct answer
 var showAnswer = function(){
   var checkA = isCorrectChoice();
   $("#choices").empty();
@@ -52,10 +115,10 @@ var showAnswer = function(){
     $("#question").text("Sorry! The correct answer is: " + answer);
   }
   $("#choices").append("<img src='" + imgSrc + "'>");
-  setTimeout(reset,5000);
+  setTimeout(nextQ,5000);
 }
 // ========================================
-// Check if correct answer checked
+// Check if correct answer selected
 
 var isCorrectChoice = function (){
   var userSelected = $('input[name=radioSelect]:checked','#choices').val();
@@ -73,51 +136,7 @@ var isCorrectChoice = function (){
   }
 }
 
-// =================================
-// Questions
-var questionArray = [
-  {question: "This is the first Question.",
-	choices: ["Choice a", "Choice b", "Choice c", "Choice d"],
-	correctChoice: "Choice c",
-	image: "https://static.fjcdn.com/gifs/Cat_439f66_783457.gif"},
-  {question: "This is the second Question.",
-	choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
-	correctChoice: "Choice 1",
-	image: "http://www.dream-wallpaper.com/free-wallpaper/nature-wallpaper/waterfall-and-stream-2-wallpaper/1280x1024/free-wallpaper-11.jpg"},
-  {question: "This is the third Question.",
-  choices: ["Choice i", "Choice ii", "Choice iii", "Choice iv"],
-  correctChoice: "Choice iv",
-  image: "http://images.all-free-download.com/images/graphiclarge/beautiful_natural_scenic_03_hd_picture_166230.jpg"}
-]
-// ==================================
-// Counters
-var numCorrect = 0;
-var numIncorrect = 0;
-var unanswered = 0;
-var count = 0;
-var answer = "";
-var imgSrc = "";
-
 // ==============================
-// Create radio buttons populated with choices
-
-var displayQnA = function(currentQ){
-  $("#question").text(currentQ.question);
-  for(var i = 0; i < currentQ.choices.length; i++){
- 
- 	var option =$("<label>");
- 	option.html("<input type='radio' name='radioSelect' value ='" + 
- 	currentQ.choices[i] + "'>" + currentQ.choices[i]);
- 	$("#choices").append(option);
-  }
-  $("label").after("</br>");
-  var btn = $("<button id='verify' type='button'>").addClass("btn btn-lg btn-primary").text("Submit");
-  $("#choices").append(btn);
-  answer = currentQ.correctChoice;
-  imgSrc = currentQ.image;
-}
-
-// ============================
 // Submit button event
 $(document).on("click", "#verify", function(){
   timeOut = true;
@@ -125,23 +144,9 @@ $(document).on("click", "#verify", function(){
   showAnswer();
 });
 //  ================
-// Start game
-$(document).on("click", ".start", function(){
-  start();
+// Start button and restart button
+$(document).on("click", ".reset", function(){
+  reset();
 });
 
-var start = function(){
-numCorrect = 0;
-numIncorrect = 0;
-unanswered = 0;
-count = 0;
-answer = "";
-imgSrc = "";
-timeOut = false;
-time = 30;
-$("#choices").empty();
-$("#timer").text("Time Remaining: " + time);
-timer();
-displayQnA(questionArray[count]);
-}
 
